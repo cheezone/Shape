7ac20ebed863e0f21d44c0e7129e117cc229b454
cheezone <41314020+cheezone@users.noreply.github.com>
2024-04-06 23:14:45 +0800

    feat: test

--- src/shapes/base.shape.ts
+++ src/shapes/base.shape.ts
@@ -1,9 +1,12 @@
-import type { ShapeEnum, ShapeInstance, ShapeType } from '../constant'
+import type { ShapeInstance } from '../constant'
 import { RelationManager } from '../relation'
-import { RelationEnum, RelationType } from '../relation/type'
 
 export abstract class Shape {
   public in<T extends ShapeInstance, TRight extends ShapeInstance>(this: T, other: TRight) {
-    return RelationManager.runImpl('In', this, other) 
+    return RelationManager.runImpl('In', this, other)
+  }
+
+  public contains<T extends ShapeInstance, TRight extends ShapeInstance>(this: T, other: TRight) {
+    return RelationManager.runImpl('In', other, this)
   }
 }
---
+++ src/util/index.ts
@@ -0,0 +1,14 @@
+/**
+ * 检查给定的值是否在给定的误差范围内相等。
+ *
+ * 如果两个数字的差小于 `epsilon`，则它们在给定的误差范围内相等。
+ *
+ * @param a - 第一个值。
+ * @param b - 第二个值。
+ * @param epsilon - 误差范围，默认为 0.0001。
+ *
+ * @returns 如果两个值在给定的误差范围内相等，则返回 `true`，否则返回 `false`。
+ */
+export function fuzzyEqual(a: number, b: number, epsilon: number = 0.0001): boolean {
+  return Math.abs(a - b) < epsilon
+}
